dfe8d41049ccdb4ce1d24c7c18b27cb045ff82a7
hydrapon <panchehin43@yandex.ru>
2018-07-19 17:36:11 +0300

    create

---
+++ controllers/AppController.php
@@ -0,0 +1,14 @@
+<?php
+
+
+
+namespace app\controllers;
+
+/**
+ * Description of AppController
+ *
+ * @author hydra
+ */
+class AppController extends \yii\web\Controller{
+    //put your code here
+}
---
+++ controllers/PostController.php
@@ -0,0 +1,30 @@
+<?php
+
+
+
+namespace app\controllers;
+use app\models\Post;
+/**
+ * Description of PostController
+ *
+ * @author hydra
+ */
+class PostController extends AppController {
+    
+    public function actionIndex() {
+        //$posts = Post::find()->select('id, title, excerpt')->all();
+        
+        $query = Post::find()->select('id, title, excerpt')->orderBy('id DESC');
+        $pages = new \yii\data\Pagination(['totalCount' => $query->count(), 'pageSize' => 5, 'pageSizeParam' => false, 'forcePageParam' => false]);
+        $posts = $query->offset($pages->offset)->limit($pages->limit)->all();
+        
+        return $this->render('index', compact('posts', 'pages'));
+    }
+    public function actionView() {
+        $id = \Yii::$app->request->get('id');
+        $post = Post::findOne($id);
+        if(empty($post)) throw new \yii\web\HttpException(404, 'Такой страницы нет..');
+        return $this->render('view', compact('post'));
+    }
+    
+}
